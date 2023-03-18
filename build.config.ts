94700e2f2171fb69d999ae19b228d792d57bf93d
Yaël Guilloux <yael.guilloux@gmail.com>
2023-03-18 16:24:28 +0100

    chore(ignore): fix ignored

---
+++ build.config.ts
@@ -0,0 +1,23 @@
+import { defineBuildConfig } from 'unbuild'
+
+export default defineBuildConfig({
+  entries: [
+    'src/index',
+    'src/vue',
+  ],
+  failOnWarn: false,
+  clean: false,
+  declaration: true,
+  rollup: {
+    emitCJS: true,
+  },
+  externals: [
+    'vite',
+    '@vue',
+    '@vue/*',
+    '@vue/compiler-sfc',
+    '@vue/compiler-core',
+    'vue/compiler-sfc',
+    'vue',
+  ],
+})
